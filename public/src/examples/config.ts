0e4e9d26fed309aa3656fc73d0589f9b0492d341
Maaz Uddin <maazuddin041@gmail.com>
2023-10-09 13:53:03 +0100

    Adding examples for subaccounts

---
+++ public/src/examples/config.ts
@@ -0,0 +1,3 @@
+export const TEST_ACCT_KEY = "cigar tip purchase gym income crumble short hobby model rocket push twelve"  # public key 0x6579b6b4edd3d89ba088ea9d4fcdc3d9e030605054ebff2def870e601bc3eb98
+export const TEST_SUB_ACCT_KEY = "7540d48032c731b3a17947b63a04763492d84aef854246d355a703adc9b54ce9"
+export const TEST_NETWORK = "SUI_STAGING"

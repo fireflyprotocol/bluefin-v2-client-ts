b5c41508475e2326a8fe1c8d1410644cc014ccef
ahsankhan201010 <ahsankhan201010@gmail.com>
2024-04-30 19:30:21 +0500

    accomodate zk login for getUsdc utility

--- src/bluefinClient.ts
+++ src/bluefinClient.ts
@@ -849,7 +849,7 @@ export class BluefinClient {
       const coin =
         await this.contractCalls.onChainCalls.getUSDCoinHavingBalance({
           amount,
-          address: this.is_wallet_extension
+          address: this.is_wallet_extension || this.isZkLogin
             ? this.walletAddress
             : this.signer.toSuiAddress(),
           currencyID: this.contractCalls.onChainCalls.getCurrencyID(),

bed9aede2b896d0bafa4c7e6afc4a6278aa62092
Yasser Zubair <yasserzubair68@gmail.com>
2024-09-16 06:15:16 +0500

    enabling trigger price

--- src/bluefinClient.ts
+++ src/bluefinClient.ts
@@ -694,7 +694,9 @@ export class BluefinClient {
       orderType: order.orderType,
       triggerPrice:
         order.orderType === ORDER_TYPE.STOP_MARKET ||
-        order.orderType === ORDER_TYPE.STOP_LIMIT
+        order.orderType === ORDER_TYPE.STOP_LIMIT ||
+        order.orderType === ORDER_TYPE.STOP_LOSS_LIMIT ||
+        order.orderType === ORDER_TYPE.TAKE_PROFIT_LIMIT
           ? order.triggerPrice || 0
           : 0,
       postOnly: orderToSign.postOnly,

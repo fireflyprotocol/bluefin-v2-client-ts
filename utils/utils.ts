a42785ba557d11785d065a3398ad3af2b9291fd9
Ahmed Riyad <ahmed.riyad@incorta.com>
2024-10-12 18:25:08 +0300

    bump lib-sui

--- utils/utils.ts
+++ utils/utils.ts
@@ -9,6 +9,8 @@ import {
   toBigNumberStr,
 } from "@firefly-exchange/library-sui";
 import fs from "fs";
+import { toHex } from "@firefly-exchange/library-sui/dist/src";
+import { decodeSuiPrivateKey } from "@mysten/sui/cryptography";
 import deploymentData from "../deployment.json";
 import CustomError from "../src/interfaces";
 import { Errors } from "../src/constants";
@@ -91,9 +93,11 @@ export async function setupTestAccounts(
 export function createWallet(): { privateKey: string; publicAddress: string } {
   const wallet = Ed25519Keypair.generate();
   const signerKey = wallet.getSecretKey();
+  const keyPair = decodeSuiPrivateKey(signerKey);
+
   const publicAddress = wallet.toSuiAddress();
   return {
-    privateKey: Buffer.from(signerKey, "base64").toString("hex"),
+    privateKey: toHex(keyPair.secretKey),
     publicAddress,
   };
 }
